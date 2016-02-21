b51ddcb299afa0f210c116ffcdd6d3475b121c4c
tlblanton <tylerblanton635@gmail.com>
2016-02-21 16:10:16 -0700

    program is finished, but remains UNpolished

--- main.py
+++ main.py
@@ -1,12 +1,69 @@
+import sys
+
+moves = 0
+
+
+def display(aCopy, bCopy, cCopy):
+    print '** Move Number: {0}'.format(moves)
+    sys.stdout.write("[")
+    for num in range(len(aCopy)):
+        sys.stdout.write('%s' % aCopy[num])
+        if num != len(aCopy)-1:
+            sys.stdout.write(", ")
+        else:
+            print ']'
+    if len(aCopy) == 0:
+        print(']')
+
+    sys.stdout.write("[")
+    for num in range(len(bCopy)):
+        sys.stdout.write('%s' % bCopy[num])
+        if num != len(bCopy)-1:
+            sys.stdout.write(", ")
+        else:
+            print(']')
+    if len(bCopy) == 0:
+        print(']')
+
+    sys.stdout.write("[")
+    for num in range(len(cCopy)):
+        sys.stdout.write('%s' % cCopy[num])
+        if num != len(cCopy)-1:
+            sys.stdout.write(", ")
+        else:
+            print ']'
+    if len(cCopy) == 0:
+        print(']')
+    print('----------------------------------------------------')
+
+
+def hanoi(ringNum, start, aux, end):
+    global moves
+    if ringNum == 1:
+        end.append(start.pop())
+        moves += 1
+        display(A, B, C)
+    else:
+        hanoi(ringNum-1, start, end, aux)
+        end.append(start.pop())
+        moves += 1
+        display(A, B, C)
+        hanoi(ringNum-1, aux, start, end)
+
 
 A = []
 B = []
 C = []
 
-rings = input("How many rings are on tower A?\n");
 
-for i in range(rings, 0):
-	A.append(rings);
+rings = input("How many rings are on tower A?\n")
+n = rings
 
-for num in A:
-	print("rings: %d" % num);
+while n:
+    A.append(n)
+    n -= 1
+
+
+display(A, B, C)
+hanoi(rings, A, B, C)
+print "Number of moves: {0}".format(moves)
